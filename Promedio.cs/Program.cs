13648e36efb37d8ae65b58828bad3bde3b3c10d5
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-08 23:41:28 -0300

    Promedio.cs

---
+++ Promedio.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Promedio.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal promedio;
+            Console.Write("Ingrese nota1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese nota2: ");
+            int b = Convert.ToInt32(Console.ReadLine());  
+            Console.Write("Ingrese nota3: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            promedio = (a + b + c) / 3;
+
+            Console.WriteLine("El primedio es: " + promedio);
+
+            Console.ReadKey();
+
+        }
+    }
+}
